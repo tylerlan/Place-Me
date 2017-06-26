37aa3d64f23f3c0eedf546eda0462cd78d4c1535
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-25 20:51:56 -0700

    Adds route handler and modules to index, and adds boom, cookie-parser, and dotenv to packages

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+"use strict";
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// CSRF protection
+app.use((req, res, next) => {
+  if (/json/.test(req.get("Accept"))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+const users = require("./routes/users");
+app.use(users);
 
 app.use((req, res) => {
   res.sendStatus(404);
@@ -9,3 +33,5 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log("Listening on port", port);
 });
+
+module.exports = app;

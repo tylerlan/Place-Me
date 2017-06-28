ead8a53289d15ab1cafbba9f0d571a93db4275d8
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-06-28 16:22:29 -0700

    Updates test to match updated seeds

--- test/seeds.test.js
+++ test/seeds.test.js
@@ -72,7 +72,25 @@ suite(
               comment_id: 1,
               user_id: 1,
               picture_id: 1,
-              comment: "this place is awesome"
+              comment: "I love to code!"
+            },
+            {
+              comment_id: 2,
+              user_id: 2,
+              picture_id: 4,
+              comment: "Javascript is the best!"
+            },
+            {
+              comment_id: 3,
+              user_id: 2,
+              picture_id: 2,
+              comment: "Fullstack for the win."
+            },
+            {
+              comment_id: 4,
+              user_id: 1,
+              picture_id: 3,
+              comment: "Who says we can't do it all?"
             }
           ];
 

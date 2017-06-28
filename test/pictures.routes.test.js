5019f84a9942eb331e8695ad39d1a0b3a72bc614
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-06-27 19:04:45 -0700

    Updates pictures test to include post and delete

--- test/pictures.routes.test.js
+++ test/pictures.routes.test.js
@@ -117,6 +117,11 @@ suite("pictures routes", () => {
       );
   });
 
+  /* TEST FOR: < < < < < < < < < < < < < < < < < < < < <
+  - User not found (404)
+  - Returns [] when user has no favorites
+  > > > > > > > > > > > > > > > > > > > > > > > > > > > */
+
   /*=================================
       GET ONE OF USERS' FAVORITES
   =================================*/
@@ -139,5 +144,152 @@ suite("pictures routes", () => {
         done
       );
   });
-  /*=================================*/
+
+  /* TEST FOR: < < < < < < < < < < < < < < < < < < < < <
+  - User not found (404)
+  - Picture not found (404)
+  > > > > > > > > > > > > > > > > > > > > > > > > > > > */
+
+  /*=================================
+        POST A USERS' FAVORITE
+
+  // User does a search
+  // User selects picture from those ~presented~
+
+  // Tries to POST that picture info ({ url: "", lat: #, lon: # }) to the pictures table -- there will be no formal route for this POST. It should just try to add the info...
+
+  // Knex checks to see if there is a picture in pictures table that has the same url as the picture object we are attempting to POST
+
+  // IF SO:
+  // // get the picture_id of that picture, and
+  // // POST { user_id: #,  picture_id: # } to favorites table
+
+  // IF NOT:
+  // // add the data { url: "", lat: #, lon: # } to pictures table, and get the picture_id of the new entry, make it available, and...
+  // // POST { user_id: #,  picture_id: # } to favorites table
+  =================================*/
+
+  test("POST /pictures/:user_id 'where picture is already in the pictures database'", done => {
+    request(server)
+      // The picture data being sent here is that of picture_id: 4 (which Tyler has already favorited)
+      .post("/pictures/1")
+      .set("Accept", "application/json")
+      .send({
+        url: "https://farm1.staticflickr.com/2/3788878_2f56283336_m.jpg",
+        lat: "56.627389",
+        lon: "-122.726777"
+      })
+      // Behind the scenes, {user_id: 1, picture_id: 4} should be added to favorites table
+      .expect("Content-Type", /json/)
+      .expect(200, [
+        {
+          picture_id: 4,
+          url: "https://farm1.staticflickr.com/2/3788878_2f56283336_m.jpg",
+          lat: "56.627389",
+          lon: "-122.726777"
+        }
+      ]) // BELOW, CHECK DATABASE TO MAKE SURE THAT {user_id: 1, picture_id: 4} WAS ADDED TO FAVORITES. THE OBJECT SHOULD HAVE THE PROPERTY favorite_id: 7
+      .end((httpErr, res) => {
+        if (httpErr) {
+          return done(httpErr);
+        }
+        knex("favorites")
+          .where({ user_id: 1, picture_id: 5 })
+          .first()
+          .then(favorite => {
+            assert.deepEqual(favorite, {
+              favorite_id: 7,
+              user_id: 1,
+              picture_id: 4
+            });
+            done();
+          })
+          .catch(dbErr => {
+            done(dbErr);
+          });
+      });
+  });
+
+  test("POST /pictures/:user_id 'where picture is NOT already in the pictures database'", done => {
+    request(server)
+      // The picture data being sent here is all new
+      .post("/pictures/1")
+      .set("Accept", "application/json")
+      .send({
+        url: "https://farm1.staticflickr.com/2/1234567_8g10111213_i.jpg",
+        lat: "12.345678",
+        lon: "-122.726777"
+      })
+      // Behind the scenes, {user_id: 1, picture_id: 5} should be added to favorites table
+      .expect("Content-Type", /json/)
+      .expect(200, [
+        {
+          picture_id: 5,
+          url: "https://farm1.staticflickr.com/2/1234567_8g10111213_i.jpg",
+          lat: "12.345678",
+          lon: "-122.726777"
+        }
+      ]) // BELOW, CHECK DATABASE TO MAKE SURE THAT {user_id: 1, picture_id: 5} WAS ADDED TO FAVORITES. THE OBJECT SHOULD HAVE THE PROPERTY favorite_id: 8
+      .end((httpErr, res) => {
+        if (httpErr) {
+          return done(httpErr);
+        }
+        knex("favorites")
+          .where({ user_id: 1, picture_id: 5 })
+          .first()
+          .then(favorite => {
+            assert.deepEqual(favorite, {
+              favorite_id: 8,
+              user_id: 1,
+              picture_id: 5
+            });
+            done();
+          })
+          .catch(dbErr => {
+            done(dbErr);
+          });
+      });
+  });
+
+  /* TEST FOR: < < < < < < < < < < < < < < < < < < < < <
+  - User not found (404)
+  > > > > > > > > > > > > > > > > > > > > > > > > > > > */
+
+  /*=================================
+        DELETE A USERS' FAVORITE
+  =================================*/
+  test("DELETE /pictures/:user_id/:picture_id", done => {
+    request(server)
+      // DELETE one of Ronan's favorites (i.e. where picture_id: 2)
+      .del("/pictures/1/2")
+      .set("Accept", "application/json")
+      .set("Content-Type", "application/json")
+      .expect(200, [
+        {
+          picture_id: 2,
+          url: "https://farm1.staticflickr.com/2/9998878_4m62283336_l.jpg",
+          lat: "44.600389",
+          lon: "-122.726307"
+        }
+      ]) // BELOW, CHECK DATABASE TO MAKE SURE THAT picture_id: 2 WAS DELETED
+      .end((httpErr, res) => {
+        if (httpErr) {
+          return done(httpErr);
+        }
+        knex("favorites")
+          .where({ user_id: 1, picture_id: 2 })
+          .first()
+          .then(favorite => {
+            assert.isTrue(favorite, "undefined");
+            done();
+          })
+          .catch(dbErr => {
+            done(dbErr);
+          });
+      });
+  });
+  /* TEST FOR: < < < < < < < < < < < < < < < < < < < < <
+  - User not found (404)
+  - Picture not found (404)
+  > > > > > > > > > > > > > > > > > > > > > > > > > > > */
 });

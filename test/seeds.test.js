ce315ae09f791c038ea148018df046f3b54253b9
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-06-28 17:06:02 -0700

    Resolves seeds test bug

--- test/seeds.test.js
+++ test/seeds.test.js
@@ -114,7 +114,7 @@ suite(
               comment_id: 1,
               user_id: 1,
               picture_id: 1,
-              comment: "this place is awesome"
+              comment: "I love to code!"
             }
           ];
 
